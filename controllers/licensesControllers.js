e1edbdfd4560e6fab3bbef89bd22ea91f73812fb
Domagoj Šimatović <dosimatovic@gmail.com>
2024-10-08 21:15:35 +0200

    Dodana nova provjera za brisanje

--- controllers/licensesControllers.js
+++ controllers/licensesControllers.js
@@ -69,7 +69,8 @@ const deleteLicense = async (req, res) => {
 
   try {
     const deletedLicense = await License.findOneAndDelete({
-      license: licenseKey,
+      licenseKey: licenseKey,
+      encryptedMachineId: encryptedMachineId,
     });
 
     if (!deletedLicense) {
